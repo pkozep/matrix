1adc5b68f1928022240460bccf58bd33d2ecfc12
pkozep <pkozep@gmail.com>
2025-09-20 16:45:01 +0300

    Реализовал метод нахождения определителя

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from random import randint
+
 class Matrix:
     def __init__( self, matrix: list[list, ] ):
         self.matrix = matrix
@@ -32,6 +34,36 @@ class Matrix:
 
     def transposition( self ):
         return Matrix( [ [ self.matrix[ i ][ j ] for i in range( self.m ) ] for j in range( self.n ) ] )
+
+    def minor( self, pos: list[ int, int ] ):
+        result_matrix = [ [] for _ in range( self.m - 1 ) ]
+        for i in range( self.m ):
+            for j in range( self.n ):
+                if i != pos[ 0 ] and j != pos[ 1 ]:
+                    if i > pos[0]:
+                        result_matrix[ i - 1 ].append( self.matrix[ i ][ j ] )
+                    else:
+                        result_matrix[ i ].append( self.matrix[ i ][ j ] )
+        return Matrix( result_matrix )
+
+    def determinant( self ):
+        if self.isSquare:
+            if self.m == 1:
+                return self.matrix[ 0 ][ 0 ]
+            elif self.m == 2:
+                return self.matrix[ 0 ][ 0 ] * self.matrix[ 1 ][ 1 ] - \
+                       self.matrix[ 0 ][ 1 ] * self.matrix[ 1 ][ 0 ]
+            elif self.m == 3:
+                return self.matrix[ 0 ][ 0 ] * self.matrix[ 1 ][ 1 ] * self.matrix[ 2 ][ 2 ] + \
+                       self.matrix[ 0 ][ 1 ] * self.matrix[ 1 ][ 2 ] * self.matrix[ 2 ][ 0 ] + \
+                       self.matrix[ 0 ][ 2 ] * self.matrix[ 1 ][ 0 ] * self.matrix[ 2 ][ 1 ] - \
+                       self.matrix[ 0 ][ 2 ] * self.matrix[ 1 ][ 1 ] * self.matrix[ 2 ][ 0 ] - \
+                       self.matrix[ 0 ][ 1 ] * self.matrix[ 1 ][ 0 ] * self.matrix[ 2 ][ 2 ] - \
+                       self.matrix[ 0 ][ 0 ] * self.matrix[ 1 ][ 2 ] * self.matrix[ 2 ][ 1 ]
+            else:
+                return sum( self.matrix[ i ][ 0 ] * ( -1 )**i * self.minor( [ i, 0 ] ).determinant() for i in range( self.m ) )
+        else:
+            raise "Определитель не определён для неквадратных матриц"
 
     @staticmethod
     def unit( order: int ):
