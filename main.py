b1bf9850c781146ee1060f395b63971f872e34a6
pkozep <pkozep@gmail.com>
2025-09-22 16:48:13 +0300

    Реализовал метод нахождения определителя Гауса

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
+from math import prod
 from random import randint
+from itertools import permutations
 
 class Matrix:
-    def __init__( self, matrix: list[list, ] ):
+    def __init__( self, matrix: list[int, ] ):
         self.matrix = matrix
         self.m, self.n = len( matrix ), len( matrix[0] )
         self.isSquare = self.m == self.n
 
     def __str__( self ) -> str:
-        max_el_len = len( str( max( max(vec) for vec in self.matrix ) ) )
+        max_el_len = len( str( max( max(vec, key=lambda x: len(str(x))) for vec in self.matrix ) ) )
 
         result = "\n"
         for vec in self.matrix:
@@ -46,24 +48,66 @@ class Matrix:
                         result_matrix[ i ].append( self.matrix[ i ][ j ] )
         return Matrix( result_matrix )
 
-    def determinant( self ):
+    @staticmethod
+    def sign_of_permutation( perm ):
+        n = len( perm )
+        inversions = 0
+        for i in range( n ):
+            for j in range( i+1, n ):
+                if perm[ i ] > perm[ j ]:
+                    inversions += 1
+        return (-1) ** inversions
+
+    def _det_formula( self ):
         if self.isSquare:
             if self.m == 1:
                 return self.matrix[ 0 ][ 0 ]
-            elif self.m == 2:
-                return self.matrix[ 0 ][ 0 ] * self.matrix[ 1 ][ 1 ] - \
-                       self.matrix[ 0 ][ 1 ] * self.matrix[ 1 ][ 0 ]
-            elif self.m == 3:
-                return self.matrix[ 0 ][ 0 ] * self.matrix[ 1 ][ 1 ] * self.matrix[ 2 ][ 2 ] + \
-                       self.matrix[ 0 ][ 1 ] * self.matrix[ 1 ][ 2 ] * self.matrix[ 2 ][ 0 ] + \
-                       self.matrix[ 0 ][ 2 ] * self.matrix[ 1 ][ 0 ] * self.matrix[ 2 ][ 1 ] - \
-                       self.matrix[ 0 ][ 2 ] * self.matrix[ 1 ][ 1 ] * self.matrix[ 2 ][ 0 ] - \
-                       self.matrix[ 0 ][ 1 ] * self.matrix[ 1 ][ 0 ] * self.matrix[ 2 ][ 2 ] - \
-                       self.matrix[ 0 ][ 0 ] * self.matrix[ 1 ][ 2 ] * self.matrix[ 2 ][ 1 ]
             else:
-                return sum( self.matrix[ i ][ 0 ] * ( -1 )**i * self.minor( [ i, 0 ] ).determinant() for i in range( self.m ) )
+                perms = list( permutations( range( self.n ) ) )
+    
+                det = 0
+                for perm in perms:
+                    product = 1
+                    for i in range( self.n ):
+                        product *= self.matrix[ i ][ perm[ i ] ]
+                        
+                    det += Matrix.sign_of_permutation( perm ) * product
+                return det
+
+    def _det_minors( self, k: int=0 ):
+        if self.n == 1:
+            return self.matrix[ 0 ][ 0 ]
         else:
-            raise "Определитель не определён для неквадратных матриц"
+            return sum( self.matrix[ i ][ 0 ] * ( -1 )**i * self.minor( [ i, 0 ] ).determinant() for i in range( self.m ) )
+
+    def _get_vector( self, k: int ) -> list[int,]:
+        return self.matrix[ k ]
+
+    def _put_vector( self, k: int, vector: list ) -> None:
+        self.matrix = [ self.matrix[i] if i != k else vector for i in range( self.m ) ] 
+
+    def _change_vector( self, k1, k2 ):
+        vector1 = self.matrix[ k1 ]
+        vector2 = self.matrix[ k2 ]
+        self.matrix[ k1 ], self.matrix[ k2 ] = vector2, vector1
+
+    def _det_gaus( self ):
+        if self.isSquare:
+            if self.m == 1:
+                return self.matrix[ 0 ][ 0 ]
+            else:
+                other = Matrix( self.matrix )
+                for i in range( other.n - 1 ):
+                    for j in range( 1, other.n - i ):
+                        a = - other.matrix[ i + j ][ i ] / other.matrix[ i ][ i ]
+                        other._put_vector( i + j, [ other._get_vector( i + j )[ n ] + other._get_vector( i )[ n ] * a for n in range( other.n ) ] )
+
+        return prod( [ other.matrix[ i ][ i ] for i in range( other.m ) ] ) 
+
+    def determinant( self, method: str="formula" ):
+        if method == "formula": return self._det_formula()
+        elif method == "minors":    return self._det_minors()
+        elif method == "gaus":  return self._det_gaus()
 
     @staticmethod
     def unit( order: int ):
