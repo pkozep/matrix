6ba06aac04585c2584507aa8c5f07b84e11d19c0
pkozep <pkozep@gmail.com>
2025-09-22 22:29:37 +0300

    Переработал представление матрицы в виде элементов векторов

--- main.py
+++ main.py
@@ -3,113 +3,119 @@ from random import randint
 from itertools import permutations
 
 class Matrix:
-    def __init__( self, matrix: list[int, ] ):
-        self.matrix = matrix
-        self.m, self.n = len( matrix ), len( matrix[0] )
+    class Vector:
+        def __init__( self, vec: list[int, ] ):
+            self.vec = vec
+            self.n = len( vec )
+
+        def __add__( self, other ):
+            if isinstance( other, Matrix.Vector ):
+                return Matrix.Vector( [ self.vec[ i ] + other.vec[ i ] for i in range( self.n ) ] )
+            
+        def __mul__( self, other ):
+            if isinstance( other, (int, float) ):
+                return Matrix.Vector( [ other * self.vec[ i ] for i in range( self.n )] )
+        
+    def __init__( self, matr: list[Vector, ] ):
+        if isinstance( matr[ 0 ], Matrix.Vector ):
+            self.m, self.n = len( matr ), matr[0].n
+            self.matr = matr
+        else:
+            self.m, self.n = len( matr ), len( matr[ 0 ] )
+            self.matr = [ Matrix.Vector( vec ) for vec in matr ]
         self.isSquare = self.m == self.n
+        if self.isSquare:
+            self.order = self.n
 
     def __str__( self ) -> str:
-        max_el_len = len( str( max( max(vec, key=lambda x: len(str(x))) for vec in self.matrix ) ) )
-
+        MAX_LEN_EL = len( str( max( max( vec.vec, key=lambda x: len( str( x ) ) ) for vec in self.matr ) ) )
         result = "\n"
-        for vec in self.matrix:
-            result += " ".join( [ " " * ( max_el_len - len( str( el ) )) + str( el ) for el in vec ] ) + '\n'
-
+        for vec in self.matr:
+            result += " ".join( [ " " * ( MAX_LEN_EL - len( str( el ) )) + str( el ) for el in vec.vec ] ) + '\n'
         return result
-
+    
     def __add__( self, other ):
         if isinstance( other, Matrix ):
-            return Matrix( [ [ self.matrix[ i ][ j ] + other.matrix[ i ][ j ] for j in range( self.n ) ] for i in range( self.m ) ] )
-        else:
-            raise "Матрицы можно складывать только с матрицами"
+            return Matrix( [ self.matr[ i ] + other.matr[ i ] for i in range( self.m ) ] )
 
     def __mul__( self, other ):
-        if isinstance( other, Matrix ):
-            if self.n == other.m:
-                return Matrix( [ [ sum( self.matrix[ i ][ k ] * other.matrix[ k ][ j ] for k in range( self.n ) ) for j in range( other.n ) ] for i in range( self.m ) ] )
-            else:
-                raise "Для умножения матриц количество столбцов первой матрицы, должно быть равно количеству строк второй матрицы"
-        elif isinstance( other, ( int, float ) ):
-            return Matrix( [ [ self.matrix[ i ][ j ] * other for j in range( self.n ) ] for i in range( self.m ) ] )
-        else:
-            raise "Матрицы можно умножать только на матрицы, либо числа"
+        if isinstance( other, ( int, float ) ):
+            return Matrix( [ vec * other for vec in range( self.m ) ] )
+        elif isinstance( other, Matrix ):
+            return Matrix( [ [ sum( self.matr[ i ].vec[ k ] * other.matr[ k ].vec[ j ] for k in range( self.n ) ) for j in range( other.n ) ] for i in range( self.m ) ] )
 
-    def transposition( self ):
-        return Matrix( [ [ self.matrix[ i ][ j ] for i in range( self.m ) ] for j in range( self.n ) ] )
+    def _transposition( self ):
+        return Matrix( [ [ self.matr[ i ].vec[ j ] for i in range( self.m ) ] for j in range( self.n ) ] )
 
-    def minor( self, pos: list[ int, int ] ):
-        result_matrix = [ [] for _ in range( self.m - 1 ) ]
+    def _minor( self, pos: list[ int, int ] ):
+        result_matrix = []
         for i in range( self.m ):
-            for j in range( self.n ):
-                if i != pos[ 0 ] and j != pos[ 1 ]:
-                    if i > pos[0]:
-                        result_matrix[ i - 1 ].append( self.matrix[ i ][ j ] )
-                    else:
-                        result_matrix[ i ].append( self.matrix[ i ][ j ] )
+            if i != pos[ 0 ]:
+                result_matrix.append( [] )
+                for j in range( self.n ):
+                    if j != pos[ 1 ]:
+                        result_matrix[ -1 ].append( self.matr[ i ].vec[ j ] )
         return Matrix( result_matrix )
+    
+    def determinant( self, methods: str="formula" ) -> int | float:
+        if methods == "formula":
+            return Matrix._det_formula( self.matr )
+        elif methods == "minors":
+            return Matrix._det_minors( self.matr )
+        elif methods == "gaus":
+            return Matrix._det_gaus( self.matr )
 
     @staticmethod
-    def sign_of_permutation( perm ):
+    def _det_formula( matrix: list=[ list, ] ) -> int | float:
+        obj = Matrix( matrix )
+        if obj.isSquare:
+            if obj.order == 1:
+                return obj.matr[ 0 ].vec[ 0 ]
+            else:
+                perms = list( permutations( range( obj.order ) ) )
+                det = 0
+                for J in range( len( perms ) ):
+                    det += ( -1 )**Matrix._invers( perms[ J ] ) * prod( [ obj.matr[ i ].vec[ perms[ J ][ i ] ] for i in range( obj.order ) ] )
+                return det
+
+    @staticmethod
+    def _det_minors( matrix: list=[ list, ] ) -> int | float:
+        obj = Matrix( matrix )
+        if obj.isSquare:
+            if obj.order == 1:
+                return obj.matr[ 0 ].vec[ 0 ]
+            else:
+                return sum( ( -1 )**i * obj.matr[ i ].vec[ 0 ] * obj._minor( [ i, 0 ] ).determinant( "minors" ) for i in range( obj.order ) )
+
+    @staticmethod
+    def _det_gaus( matrix: list=[ list, ] ) -> int | float:
+        obj = Matrix( matrix )
+        if obj.isSquare:
+            if obj.order == 1:
+                return obj.matr[ 0 ].vec[ 0 ]
+            else:
+                for i in range( obj.order - 1 ):
+                    for j in range( 1, obj.order - i ):
+                        a = - obj.matr[ i + j ].vec[ i ] / obj.matr[ i ].vec[ i ]
+                        obj.matr[ i + j ] += obj.matr[ i ] * a
+
+                return prod( [ obj.matr[ i ].vec[ i ] for i in range( obj.order ) ] ) 
+
+    @staticmethod
+    def _invers( perm: list[ int, ] ):
         n = len( perm )
         inversions = 0
         for i in range( n ):
             for j in range( i+1, n ):
                 if perm[ i ] > perm[ j ]:
                     inversions += 1
-        return (-1) ** inversions
-
-    def _det_formula( self ):
-        if self.isSquare:
-            if self.m == 1:
-                return self.matrix[ 0 ][ 0 ]
-            else:
-                perms = list( permutations( range( self.n ) ) )
-    
-                det = 0
-                for perm in perms:
-                    product = 1
-                    for i in range( self.n ):
-                        product *= self.matrix[ i ][ perm[ i ] ]
-                        
-                    det += Matrix.sign_of_permutation( perm ) * product
-                return det
-
-    def _det_minors( self, k: int=0 ):
-        if self.n == 1:
-            return self.matrix[ 0 ][ 0 ]
-        else:
-            return sum( self.matrix[ i ][ 0 ] * ( -1 )**i * self.minor( [ i, 0 ] ).determinant() for i in range( self.m ) )
-
-    def _get_vector( self, k: int ) -> list[int,]:
-        return self.matrix[ k ]
-
-    def _put_vector( self, k: int, vector: list ) -> None:
-        self.matrix = [ self.matrix[i] if i != k else vector for i in range( self.m ) ] 
-
-    def _change_vector( self, k1, k2 ):
-        vector1 = self.matrix[ k1 ]
-        vector2 = self.matrix[ k2 ]
-        self.matrix[ k1 ], self.matrix[ k2 ] = vector2, vector1
-
-    def _det_gaus( self ):
-        if self.isSquare:
-            if self.m == 1:
-                return self.matrix[ 0 ][ 0 ]
-            else:
-                other = Matrix( self.matrix )
-                for i in range( other.n - 1 ):
-                    for j in range( 1, other.n - i ):
-                        a = - other.matrix[ i + j ][ i ] / other.matrix[ i ][ i ]
-                        other._put_vector( i + j, [ other._get_vector( i + j )[ n ] + other._get_vector( i )[ n ] * a for n in range( other.n ) ] )
-
-        return prod( [ other.matrix[ i ][ i ] for i in range( other.m ) ] ) 
-
-    def determinant( self, method: str="formula" ):
-        if method == "formula": return self._det_formula()
-        elif method == "minors":    return self._det_minors()
-        elif method == "gaus":  return self._det_gaus()
+        return inversions
 
     @staticmethod
     def unit( order: int ):
         return Matrix( [ [ int( i == j ) for j in range( order ) ] for i in range( order ) ] )
+
+    @staticmethod
+    def random( m: int, n: int, diap: list[ int, int ]=[ -10, 10 ]):
+        return Matrix( [ [ randint( *diap ) for j in range( n ) ] for i in range( m ) ] )
     
